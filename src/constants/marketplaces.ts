3de230735c6d7ccb74602bdaa378b5b2114716de
PacificCoastBits <jimxnw@protonmail.com>
2022-05-11 16:39:10 -0700

    Adds prettierr config

--- src/constants/marketplaces.ts
+++ src/constants/marketplaces.ts
@@ -8,4 +8,5 @@ export const MarketplaceMap = new Map<string, string>([
   ["CJsLwbP1iu5DuUikHEJnLfANgKy6stB2uFgvBBHoyxwz", "Solanart"],
   ["A7p8451ktDCHq5yYaHczeLMYsjRsAkzc3hCXcSrwYHU7", "DigitalEyes"],
   ["AmK5g2XcyptVLCFESBCJqoSfwV3znGoVYQnqEnaAZKWn", "ExchangeArt"],
+  ["hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk", "Metaplex Auction House"] // This is a crutch - I need to find a new way to find the market of apps using this contract
 ]);
