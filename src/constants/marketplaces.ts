c93f6f27cc602716fa510f1b197e51fb8bbe160d
PacificCoastBits <jimxnw@protonmail.com>
2022-05-16 16:32:31 -0700

    changes auction house name in map

--- src/constants/marketplaces.ts
+++ src/constants/marketplaces.ts
@@ -2,11 +2,14 @@
 
 //I shuold probably use an interface for this.. map is probably overkill
 export const MarketplaceMap = new Map<string, string>([
-  ["M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K", "MagicEden"],
-  ["HZaWndaNWHFDd9Dhk5pqUUtsmoBCqzb1MLu3NAh1VX6B", "AlphaArt"],
-  ["617jbWo616ggkDxvW1Le8pV38XLbVSyWY8ae6QUmGBAU", "Solsea"],
-  ["CJsLwbP1iu5DuUikHEJnLfANgKy6stB2uFgvBBHoyxwz", "Solanart"],
-  ["A7p8451ktDCHq5yYaHczeLMYsjRsAkzc3hCXcSrwYHU7", "DigitalEyes"],
-  ["AmK5g2XcyptVLCFESBCJqoSfwV3znGoVYQnqEnaAZKWn", "ExchangeArt"],
-  ["hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk", "Metaplex Auction House"] // This is a crutch - I need to find a new way to find the market of apps using this contract
+    ["M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K", "MagicEden"],
+    ["HZaWndaNWHFDd9Dhk5pqUUtsmoBCqzb1MLu3NAh1VX6B", "AlphaArt"],
+    ["617jbWo616ggkDxvW1Le8pV38XLbVSyWY8ae6QUmGBAU", "Solsea"],
+    ["CJsLwbP1iu5DuUikHEJnLfANgKy6stB2uFgvBBHoyxwz", "Solanart"],
+    ["A7p8451ktDCHq5yYaHczeLMYsjRsAkzc3hCXcSrwYHU7", "DigitalEyes"],
+    ["AmK5g2XcyptVLCFESBCJqoSfwV3znGoVYQnqEnaAZKWn", "ExchangeArt"],
+    [
+        "hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk",
+        "Auction House - Contract/Not a marketplace",
+    ], // This is a crutch - I need to find a new way to find the market of apps using this contract same as the Tokenkeg ones
 ]);
